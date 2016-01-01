f27159cb88768dcd999c1a19442adee83fcff46e
stephen-fox <stephen.j.fox.jr@gmail.com>
2015-12-31 23:53:05 -0500

    Initial commit.

---
+++ threshold/Processes/Process.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threshold.Processes
+{
+    public class Process
+    {
+        public string processId;
+        public bool isListening;
+
+        public Process(string pid)
+        {
+            processId = pid;
+        }
+
+    }
+}
