7f9285c8076875b10ad378d4c79b76750d3dab25
stephen-fox <stephen.j.fox.jr@gmail.com>
2016-01-02 11:27:25 -0500

    Re-structure the project to proivde better context. Trying to follow the philosophies outlined in 'Clean Code'.

---
+++ threshold/Types/Transport/Ports.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace threshold.Types.Transport
+{
+    class Ports
+    {
+        public Dictionary<int, List<string>> ActivePorts = 
+            new Dictionary<int, List<string>>();
+
+        private void getActivePorts()
+        {
+
+        }
+
+    }
+}
